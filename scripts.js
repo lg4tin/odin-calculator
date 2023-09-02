901c47ec730c857ddcd8b68bec3bc67f77b867b4
lg4tin <lg4tin@gmail.com>
2023-09-02 14:57:41 -0400

    Add operate function to operator buttons and add some css

--- scripts.js
+++ scripts.js
@@ -1,3 +1,55 @@
+let displayValue = document.querySelector('.display-value');
+const operators = document.querySelectorAll('.operator');
+let equalsButton = document.querySelector('.equals');
+let operator;
+let a;
+let b;
+let previousCalculation;
+
+let numberButtons = document.querySelectorAll('.buttons');
+numberButtons.forEach(button => {
+    button.addEventListener('click', function() {
+        if (operator) {
+            
+            displayValue.textContent += button.textContent;
+        } else {
+            displayValue.textContent += button.textContent;
+        }
+    })
+})
+
+operators.forEach(oper => {
+    oper.addEventListener('click', function() {
+        if (a != undefined && operator != undefined) {
+            displayValue.textContent = operate(operator, a, b);
+        } else {
+            operator = oper.textContent;
+            a = +displayValue.textContent;
+            displayValue.textContent = '';
+        }
+        
+    });
+})
+
+equalsButton.addEventListener('click', function() {
+    if (b === undefined) b = +displayValue.textContent;
+    
+    displayValue.textContent = operate(operator, a, b);
+    a = operate(operator, a, b);
+    b = undefined;
+    previousCalculation = operate(operator, a, b);
+    reset();
+})
+
+function operate(operator, a, b) {
+    if (b === undefined) b = displayValue.textContent;
+    if (operator == '+') return add(a, b);
+    else if (operator == '-') return subtract(a, b);
+    else if (operator == '*') return multiply(a, b);
+    else if (operator == '/' && b === 0) return 'LMAO';
+    else if (operator == '/') return divide(a, b);
+}
+
 function add(a, b) {
     return +a + +b;
 }
@@ -14,49 +66,6 @@ function divide(a, b) {
     return a / b;
 }
 
-let displayValue = document.querySelector('.display-value');
-displayValue.textContent = '';
-const operators = document.querySelectorAll('.operator');
-let equalsButton = document.querySelector('.equals');
-let operator;
-let a;
-let b;
-
-let numberButtons = document.querySelectorAll('.buttons');
-numberButtons.forEach(button => {
-    button.addEventListener('click', function() {
-        if (operator) {
-            
-            displayValue.textContent += button.textContent;
-        } else {
-            displayValue.textContent += button.textContent;
-        }
-    })
-})
-
-operators.forEach(oper => {
-    oper.addEventListener('click', function() {
-        if (a !== undefined && b !== undefined) {
-            displayValue.textContent = operate(operator, a, b);
-        }
-        operator = oper.textContent;
-        a = +displayValue.textContent;
-        displayValue.textContent = '';
-    });
-})
-
-equalsButton.addEventListener('click', function() {
-    if (b === undefined || a !== undefined) b = +displayValue.textContent;
-    displayValue.textContent = operate(operator, a, b);
-})
-
-function operate(operator, a, b) {
-    if (operator == '+') return add(a, b);
-    else if (operator == '-') return subtract(a, b);
-    else if (operator == '*') return multiply(a, b);
-    else if (operator == '/' && b === 0) return 'LMAO';
-    else if (operator == '/') return divide(a, b);
-}
 
 document.querySelector('.clear').addEventListener('click', clear);
 
